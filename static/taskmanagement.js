c5139f332390cb1792d50fb2aa0b4e9905f51f82
matthew28845 <43422956+matthew28845@users.noreply.github.com>
2025-11-04 14:51:37 -0600

    make startTimer() function take website parameter

--- static/taskmanagement.js
+++ static/taskmanagement.js
@@ -1,20 +1,19 @@
-function startTimer() {
+function startTimer(website) {
     const timerDisplay = document.getElementById('timer')
     let secondsRemaining = 300
     document.getElementById('startbutton').disabled = true
     document.getElementById('startbutton').innerHTML = 'Test in Progress...'
-    canvaWindow = window.open('https://canva.com')
+    canvaWindow = window.open(website)
     const timerInterval = setInterval(() => {
         const minutes = Math.floor(secondsRemaining / 60)
         const seconds = secondsRemaining % 60
         timerDisplay.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
         document.title = `Time Remaining: ${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
-        
+        //redirect back when time is up
         if (secondsRemaining <= 0) {
             clearInterval(timerInterval)
             canvaWindow.close()
             alert('Time is up! You will now be redirected to the questionnaire.')
-            // Optionally, you can redirect the user or disable inputs here
         }
         
         secondsRemaining--
